59b145640af678651275c1161ec8989cee217c6c
jgroen <josh.groen@gmail.com>
2023-10-26 11:36:40 -0400

    Fixed the start_ipsec.sh script and added ipsec.conf and ipsec.secrets templates 	new file:   ipsec.conf 	new file:   ipsec.secrets 	modified:   start_ipsec.sh

--- start_ipsec.sh
+++ start_ipsec.sh
@@ -1,39 +1,48 @@
 # ARGS:
-# 1 - eNB machine id
+# 1 - gNB machine id
 # 2 - RIC machine id
 
 
 #!/bin/bash
 
-IPeNB=`sshpass -p "scope" ssh $1 'ifconfig col0 | grep '"'"'inet addr'"'"' | cut -d: -f2 | awk '"'"'{print $1}'"'"''`
-echo "eNB col0 IP $IPeNB"
+sshpass -p "scope" scp ./ipsec.conf $1:/etc/ipsec.conf
+sshpass -p "scope" scp ./ipsec.secrets $1:/etc/ipsec.secrets
+sshpass -p "ChangeMe" scp ./ipsec.conf $2:/etc/ipsec.conf
+sshpass -p "ChangeMe" scp ./ipsec.secrets $2:/etc/ipsec.secrets
 
-IPRRIC=`sshpass -p "scope" ssh $2 'ifconfig col0 | grep '"'"'inet addr'"'"' | cut -d: -f2 | awk '"'"'{print $1}'"'"''`
+IPgNB=`sshpass -p "scope" ssh $1 'ifconfig col0 | grep '"'"'inet addr'"'"' | cut -d: -f2 | awk '"'"'{print $1}'"'"''`
+echo "gNB col0 IP $IPgNB"
+
+IPRIC=`sshpass -p "ChangeMe" ssh $2 'ifconfig col0 | grep '"'"'inet addr'"'"' | cut -d: -f2 | awk '"'"'{print $1}'"'"''`
 echo "RIC col0 IP $IPRIC"
 
+sleep 2
 echo "*** Starting IPsec ***"
 #Edit /etc/ipsec.secrets
-sshpass -p "scope" ssh $1 "sed -i 's/10.207.208.180/${IPeNB}/;s/10.207.208.18/${IPRIC}/' /etc/ipsec.secrets"
-sshpass -p "scope" ssh $2 "sed -i 's/10.207.208.180/${IPRIC}/;s/10.207.208.18/${IPeNB}/' /etc/ipsec.secrets"
+sshpass -p "scope" ssh $1 "sed -i 's/10.207.208.180/${IPgNB}/;s/10.207.208.18/${IPRIC}/' /etc/ipsec.secrets"
+sshpass -p "ChangeMe" ssh $2 "sed -i 's/10.207.208.180/${IPRIC}/;s/10.207.208.18/${IPgNB}/' /etc/ipsec.secrets"
 
+sleep 2
 #Edit /etc/ipsec.conf 
-sshpass -p "scope" ssh $1 "sed -i 's/10.207.208.180/${IPeNB}/;s/10.207.208.18/${IPRIC}/' /etc/ipsec.conf"
-sshpass -p "scope" ssh $2 "sed -i 's/10.207.208.180/${IPeNB}/;s/10.207.208.18/${IPRIC}/' /etc/ipsec.conf"
+sshpass -p "scope" ssh $1 "sed -i 's/10.207.208.180/${IPgNB}/;s/10.207.208.18/${IPRIC}/' /etc/ipsec.conf"
+sshpass -p "ChangeMe" ssh $2 "sed -i 's/10.207.208.180/${IPgNB}/;s/10.207.208.18/${IPRIC}/' /etc/ipsec.conf"
 
 sshpass -p "scope" ssh $1 "sed -i 's/ike=aes256ccm128/#ike=aes256ccm128/;s/esp=aes256ccm128/#esp=aes256ccm128/' /etc/ipsec.conf"
-sshpass -p "scope" ssh $2 "sed -i 's/ike=aes256ccm128/#ike=aes256ccm128/;s/esp=aes256ccm128/#esp=aes256ccm128/' /etc/ipsec.conf"
+sshpass -p "ChangeMe" ssh $2 "sed -i 's/ike=aes256ccm128/#ike=aes256ccm128/;s/esp=aes256ccm128/#esp=aes256ccm128/' /etc/ipsec.conf"
 
 sshpass -p "scope" ssh $1 "sed -i 's/#ike=aes256gcm128/ike=aes256gcm128/;s/#esp=aes256gcm128/esp=aes256gcm128/' /etc/ipsec.conf"
-sshpass -p "scope" ssh $2 "sed -i 's/#ike=aes256gcm128/ike=aes256gcm128/;s/#esp=aes256gcm128/esp=aes256gcm128/' /etc/ipsec.conf"
+sshpass -p "ChangeMe" ssh $2 "sed -i 's/#ike=aes256gcm128/ike=aes256gcm128/;s/#esp=aes256gcm128/esp=aes256gcm128/' /etc/ipsec.conf"
 
+sleep 2
 #Allow the kernel to access ipsec by Running: 
 sshpass -p "scope" ssh $1 "apparmor_parser -R /etc/apparmor.d/usr.lib.ipsec.stroke" 
-sshpass -p "scope" ssh $2 "apparmor_parser -R /etc/apparmor.d/usr.lib.ipsec.stroke" 
+sshpass -p "ChangeMe" ssh $2 "apparmor_parser -R /etc/apparmor.d/usr.lib.ipsec.stroke" 
 
+sleep 2
 #Verify ipsec tunnel 
 sshpass -p "scope" ssh $1 "ipsec restart" 
-sshpass -p "scope" ssh $2 "ipsec restart" 
+sshpass -p "ChangeMe" ssh $2 "ipsec restart" 
 sleep 5
 sshpass -p "scope" ssh $1 "ipsec status" 
-sshpass -p "scope" ssh $2 "ipsec status" 
+sshpass -p "ChangeMe" ssh $2 "ipsec status" 
 
