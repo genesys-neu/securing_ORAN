393e8463546d8aa0018da49f2403b6a7da3d1d0e
Joshua Groen <josh.groen@gmail.com>
2024-02-13 10:12:00 -0500

    Update OFH_MACsec.sh
    made passwords generic

--- OFH_MACsec.sh
+++ OFH_MACsec.sh
@@ -17,20 +17,20 @@ IPRU='192.168.40.2'
 
 echo "*** Running without MACsec ***"
 echo "*** Starting the RU ***"
-sshpass -p "op3nran" ssh $RU "cd traffic_gen && python3 OFH_tgen.py -r -i $IPDU -f $1" &
+sshpass -p "password" ssh $RU "cd traffic_gen && python3 OFH_tgen.py -r -i $IPDU -f $1" &
 sleep 2
 
 echo "*** Starting the DU ***"
-sshpass -p "op3nran" ssh $DU "cd traffic_gen && python3 OFH_tgen.py -i $IPRU -f $1"
+sshpass -p "password" ssh $DU "cd traffic_gen && python3 OFH_tgen.py -i $IPRU -f $1"
 wait
 
 sleep 10
 
 echo "*** Running with MACsec ***"
 echo "*** Starting the RU ***"
-sshpass -p "op3nran" ssh $RU "cd traffic_gen && python3 OFH_tgen.py -r -i $MACDU -f $1" &
+sshpass -p "password" ssh $RU "cd traffic_gen && python3 OFH_tgen.py -r -i $MACDU -f $1" &
 sleep 2
 
 echo "*** Starting the DU ***"
-sshpass -p "op3nran" ssh $DU "cd traffic_gen && python3 OFH_tgen.py -i $MACRU -f $1"
+sshpass -p "password" ssh $DU "cd traffic_gen && python3 OFH_tgen.py -i $MACRU -f $1"
 wait
